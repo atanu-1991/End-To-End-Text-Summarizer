8310aa0b51d4dcc1257b77fc395e8d48e9301cef
Atanu <atanukundu1991@gmail.com>
2023-08-27 17:55:45 +0530

    model trainer updated

--- src/textSummarizer/components/model_trainer.py
+++ src/textSummarizer/components/model_trainer.py
@@ -1,6 +1,7 @@
 from transformers import TrainingArguments, Trainer
 from transformers import DataCollatorForSeq2Seq
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+from textSummarizer.entity import ModelTrainerConfig
 from datasets import load_dataset, load_from_disk
 import torch
 
